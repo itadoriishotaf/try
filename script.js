f868823fba34f354735bf66db6df96233b349968
itadoriishotaf <157963465+itadoriishotaf@users.noreply.github.com>
2024-01-28 19:39:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = 'YAYYY! I Love You so much, Nicole';
+    gif.src =
+    "https://i.pinimg.com/originals/9b/5c/f1/9b5cf1e2ce9334526c49a36b79787ee7.gif"
+});
+
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
